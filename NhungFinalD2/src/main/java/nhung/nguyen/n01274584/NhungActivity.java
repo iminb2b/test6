c9aad9b21fbd77b343e82bd138cdce7fc3ec6090
iminb2b <65203827+iminb2b@users.noreply.github.com>
2020-12-17 18:11:31 -0500

    Initial commit

---
+++ NhungFinalD2/src/main/java/nhung/nguyen/n01274584/NhungActivity.java
@@ -0,0 +1,14 @@
+package nhung.nguyen.n01274584;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NhungActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
